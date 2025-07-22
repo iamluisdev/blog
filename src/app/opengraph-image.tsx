21bf61b6ad48dd361cfe792556888d71f2ef2354
Chun-Ho (Hugo) Lin <hugo970217@gmail.com>
2025-07-23 00:15:16 +0800

    feat: add Open Graph and Twitter image generation components

---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,39 @@
+import { ImageResponse } from "next/og";
+import { join } from "node:path";
+import { readFile } from "node:fs/promises";
+
+export const alt = "Hugo Lin Dev's Open Graph Image";
+export const size = {
+  width: 2560,
+  height: 1440,
+};
+export const contentType = "image/png";
+
+export default async function Image() {
+  const logoData = await readFile(
+    join(process.cwd(), "public", "images", "opengraph-image.png"),
+  );
+  const imagePath = `data:image/png;base64,${logoData.toString("base64")}`;
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundImage: `url(${imagePath})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      ></div>
+    ),
+    {
+      ...size,
+    },
+  );
+}
