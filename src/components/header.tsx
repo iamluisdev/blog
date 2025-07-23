10a7af522a337cc2047ad02de58e31ff3f2a4a0c
Chun-Ho (Hugo) Lin <hugo970217@gmail.com>
2025-07-24 03:50:08 +0800

    feat: simplify header by removing avatar and SVG elements

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,20 +5,7 @@ function Header() {
     <header className="mt-16 mb-8">
       <div className="flex items-center">
         <Link href="/" className="flex items-center">
-          <img
-            src="/favicon.ico"
-            alt="Author avatar image"
-            className="w-10 h-10 rounded-full"
-          />
-          <svg
-            height="32"
-            width="32"
-            viewBox="0 0 32 32"
-            className="text-neutral-400"
-          >
-            <path d="M22 5L9 28" stroke="#374151"></path>
-          </svg>
-          <h1 className="text-3xl font-semibold">Dev</h1>
+          <h1 className="text-3xl font-semibold">Hugo Lin</h1>
         </Link>
       </div>
     </header>
