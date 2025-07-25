47c6431f2366c0de43a82040e4638c146de3e7c0
Luis <support@deploypath.com>
2025-07-25 11:39:12 -0700

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ function Header() {
       <FadeUp delay={0.3 * 2}>
         <div className="flex items-center">
           <Link href="/" className="flex items-center">
-            <h1 className="text-3xl font-extrabold">Hugo Lin</h1>
+            <h1 className="text-3xl font-extrabold">Luis Barragan</h1>
           </Link>
         </div>
       </FadeUp>
